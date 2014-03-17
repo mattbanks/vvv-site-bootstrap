72654128045f16e22547b18f6b560036da291ded
Matt Banks <mjbanks@gmail.com>
2014-03-17 14:16:09 -0400

    get rid of php script, use wp-cli instead

--- vvv-init.sh
+++ vvv-init.sh
@@ -19,7 +19,10 @@ define( 'WP_DEBUG', true );
 PHP
 	wp core install --allow-root --quiet --url=sitename.dev --title="SITE_NAME" --admin_user="mattbanks" --admin_password="password" --admin_email="matt@kernelcreativemedia.com"
 	cd ..
-	php wpsetup.php
+	wp option update --allow-root blogdescription ''
+	wp option update --allow-root start_of_week 0
+	wp option update --allow-root timezone_string 'America/New_York'
+	wp option update --allow-root permalink_structure '/%postname%/'
 fi
 
 # The Vagrant site setup script will restart Nginx for us
