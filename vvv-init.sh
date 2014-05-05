7d1813ec492a727925a1ae5d73d4570b72a2f668
Matt Banks <mjbanks@gmail.com>
2014-05-05 11:52:56 -0400

    add more site setup, clone default theme and activate it, install some plugins

--- vvv-init.sh
+++ vvv-init.sh
@@ -17,11 +17,44 @@ then
 	wp core config --allow-root --quiet --dbname="sitename" --dbuser=wp --dbpass=wp --dbhost="localhost" <<PHP
 define( 'WP_DEBUG', true );
 PHP
-	wp core install --allow-root --quiet --url=sitename.dev --title="SITE_NAME" --admin_user="mattbanks" --admin_password="password" --admin_email="matt@kernelcreativemedia.com"
+	wp core install --allow-root --quiet --url=sitename.dev --title="SITE_NAME" --admin_user="mattbanks" --admin_password="password" --admin_email="matt@collectivthkg.com"
+
+	# Update options
 	wp option update --allow-root --quiet blogdescription ''
 	wp option update --allow-root --quiet start_of_week 0
 	wp option update --allow-root --quiet timezone_string 'America/New_York'
 	wp option update --allow-root --quiet permalink_structure '/%postname%/'
+
+	# Delete unneeded default themes and plugins
+	wp theme delete --allow-root --quiet twentytwelve
+	wp theme delete --allow-root --quiet twentythirteen
+	wp plugin delete --allow-root --quiet hello
+
+	# Get plugins
+	wp plugin install wordpress-seo --activate
+	wp plugin install google-analytics-for-wordpress --activate
+	wp plugin install pods --activate
+
+	# Clone WordPress Starter Theme
+	cd wp-content/themes
+	git clone git@github.com:mattbanks/WordPress-Starter-Theme.git sitename > /dev/null 2>&1
+	cd sitename
+	rm -rf .git README.md CHANGELOG.md CONTRIBUTING.md
+
+	# Setup new git repo for theme
+	git init > /dev/null 2>&1
+	git add . > /dev/null 2>&1
+	git commit . -m "initial commit" > /dev/null 2>&1
+
+	# Install node/grunt packages
+	npm install > /dev/null 2>&1
+
+	# Run grunt sass to trigger style.css compilation
+	grunt sass
+
+	# Activate theme
+	wp theme activate sitename
+
 fi
 
 # The Vagrant site setup script will restart Nginx for us
